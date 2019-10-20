5f672062470e10673b8bea48fb838e160288155c
Yutta-E <wasekyo-u-8@fuji.waseda.jp>
2019-10-20 18:48:19 +0900

    list 2-3 display of query parameter

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Controller;/////
+namespace App\Controller;
 
 use App\Controller\AppController;
 
@@ -7,8 +7,12 @@ class HelloController extends AppController{
     public $autoRender = false;
 
     public function index(){
+        $id = $this->request->query['id'];
+        $pass = $this->request->query['pass'];
         echo "<html><body><h1>Hello!</h1>";
-        echo "<p>This is first commit</p></body></html>";
+        echo "<ul><li>your id: " . $id . "</li>";
+        echo "<li>password: " . $pass . "</li>";
+        echo "</body></html>";
     }
 }
 ?>
---
+++ tests/Fixture/BidrequestFixture.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * BidrequestFixture
+ */
+class BidrequestFixture extends TestFixture
+{
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    public $table = 'bidrequest';
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'biditems_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'user_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'price' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_unicode_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init()
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'biditems_id' => 1,
+                'user_id' => 1,
+                'price' => 1,
+                'created' => '2019-10-04 08:13:58'
+            ],
+        ];
+        parent::init();
+    }
+}
